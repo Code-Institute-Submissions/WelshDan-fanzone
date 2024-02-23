09ae278a2475f46bad6c52620e3b70c9f0c72dbf
WelshDan <dan.roberts78@gmail.com>
2024-02-23 12:52:32 +0000

    eslint error correction

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
+import { axiosReq } from "../../api/axiosDefaults"; //eslint-disable-next-line
 import Post from "./Post";
 import Comment from "../comments/Comment";
 
